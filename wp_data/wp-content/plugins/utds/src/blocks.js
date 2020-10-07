fcdedbdeef94c857fbfdb65c393cc7a82288f58a
Patrick Werner <pwerner2@gmail.com>
2020-10-07 13:27:52 -0400

    block filters working, need css

--- wp_data/wp-content/plugins/utds/src/blocks.js
+++ wp_data/wp-content/plugins/utds/src/blocks.js
@@ -13,3 +13,34 @@
 import './card/card.js';
 import './accordion/accordion.js';
 import './jumbotron/jumbotron.js';
+
+// DO NOT TOUCH
+wp.domReady( function() {
+	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-default',
+		label: 'Bootstrap Default',
+		isDefault: true,
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-primary',
+		label: 'Bootstrap Primary',
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-success',
+		label: 'Bootstrap Success',
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-default-lg',
+		label: 'Bootstrap Default Large',
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-primary-lg',
+		label: 'Bootstrap Primary Large',
+	} );
+	wp.blocks.registerBlockStyle( 'core/button', {
+		name: 'bootstrap-success-lg',
+		label: 'Bootstrap Success Large',
+	} );
+} );
