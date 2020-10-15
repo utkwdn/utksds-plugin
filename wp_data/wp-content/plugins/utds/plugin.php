58c700900efaa195cd81155ff767f50d236ba145
Charlie Cantrell <ccantr19@utk.edu>
2020-10-15 17:05:41 -0400

    Added documentation to explain how the PHP filter transforms the classes from Wordpress into Bootstrap format.

--- wp_data/wp-content/plugins/utds/plugin.php
+++ wp_data/wp-content/plugins/utds/plugin.php
@@ -21,6 +21,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
 
+/*
+The following code exists to bring class names on core Wordpress blocks into alignment with Bootstrap naming conventions.
+Wordpress provides filters to add custom classes to its blocks, but when using Bootstrap, there are two problems with the filter's behavior:
+
+1. It adds the classes to container div instead of the element itself
+2. The filter prepends "is-style-" to he beginning of any custom class
+
+This code removes the extraneous "is-style-" addition and puts the class on the element Bootstrap expects it to be on.
+
+Our first approach to adding Bootstrap classes to the button block was to use wp.blocks.registerBlockStyle in blocks.js. This added the classes to $block['attrs']['className'] and prepended "is-style-" to each class. While this method worked, it required creating a seperate style for every possible combination of Bootstrap button classes. This would have ended up with somewhere in the ballpark of 164 options available to choose from on the button block. This presented an overwhelming number of options for page editors, so we explored other ways to extend the button block.
+
+As of this writing, we settled on a set of three groups of radio options. The first selects color based on Bootstrap's criteria (primary, secodary, info, danger, warning, etc.). The second selects fill (full, outline, or text). The last selection offers size (small, normal, large, and block). The block editor outputs temporary classes to the container div. These temporary classes only reflect what the editor chose from the three groups of options. They're not intended to be used with CSS. They exist solely to assist the PHP filter in generating the correct Bootstrap classes.
+
+Once the selections are chosen, the block content ($block_content) and block array ($block) are passed to the PHP filter. Unfortunately, the classes from the radio selections are not stored in $block['attrs']['className']. So far, I haven't found them anywhere else in the $block array. This makes reading and modifying the selected classes more difficult than the original implementation. I read the selected classes by parsing the entire $block_content using 'wp-block-button ' as the beginning of the desired text and '">' as the end. Everything in between should be the classes from the radio selections. It splits them into an array to make them easier to work with, and reads each of the three options, outputing a class string of the correct Bootstrap class(es) based on the editor's selections. In then removes the radio selection classes from the container div and adds the Bootstrap class string to the button's a tag using 'wp-block-button__link' as a guide to know where to add the string.
+
+I anticipate a similar method will be used to add Bootstrap classes to the other core Wordpress blocks.
+*/
 add_filter( 'render_block', function( $block_content, $block ) {
 	if ( $block['blockName'] === 'core/button' ) {
 
