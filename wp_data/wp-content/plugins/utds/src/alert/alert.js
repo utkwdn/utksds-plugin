a15b58d96f47bbfd64977f8a22cf7ad380dc3e14
Michael Purdy <mp@Michaels-MacBook-Pro.local>
2020-11-13 14:00:31 -0500

    Refinements to the alert.'

--- wp_data/wp-content/plugins/utds/src/alert/alert.js
+++ wp_data/wp-content/plugins/utds/src/alert/alert.js
@@ -52,4 +52,35 @@ registerBlockType( 'alert/main', {
 	},
 } );
 
-
+ 
+/*
+Plugin Name: Sidebar example
+*/
+ ( function( wp ) {
+    var registerPlugin = wp.plugins.registerPlugin;
+    var PluginSidebar = wp.editPost.PluginSidebar;
+    var el = wp.element.createElement;
+    var Text = wp.components.TextControl;
+ 
+    registerPlugin( 'my-plugin-sidebar', {
+        render: function() {
+            return el( PluginSidebar,
+                {
+                    name: 'my-plugin-sidebar',
+                    icon: 'admin-post',
+                    title: 'My plugin sidebar',
+                },
+                el( 'div',
+                    { className: 'plugin-sidebar-content' },
+                    el( Text, {
+                        label: 'Meta Block Field',
+                        value: 'Initial value',
+                        onChange: function( content ) {
+                            console.log( 'content changed to ', content );
+                        },
+                    } )
+                )
+            );
+        }
+    } );
+} )( window.wp );
