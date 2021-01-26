0d9ac1df6026721a82d53cf2f339b7fdf8a6b3c4
Charlie Cantrell <ccantr19@utk.edu>
2021-01-26 16:01:00 -0500

    Have almost all blocks and classNames working for Card. Still need to get WP's image container HTML fixed and double check card body allowed blocks.

--- src/card/card.js
+++ src/card/card.js
@@ -1,51 +1,95 @@
 import { dispatch } from '@wordpress/data';
 import { select } from '@wordpress/data';
+import { Path, SVG } from '@wordpress/components';
 
-const { registerBlockType } = wp.blocks;
-const { InnerBlocks, InspectorControls, ColorPalette, RichText } = wp.editor;
-const { PanelBody, PanelRow, RangeControl, RadioControl } = wp.components;
+const { registerBlockType, registerBlockVariation } = wp.blocks;
+const { InnerBlocks, InspectorControls, ColorPalette, RichText, getColorObjectByColorValue } = wp.blockEditor;
+const { PanelBody, PanelRow, RangeControl, RadioControl, ToggleControl } = wp.components;
 const { withState } = wp.compose;
-const ALLOWED_BLOCKS = [ 'core/button', 'core/separator', 'card/paragraph', 'card/heading' ];
+const { ALLOWED_BLOCKS } = [ 'core/button', 'core/separator', 'card/paragraph', 'card/heading' ];
 
 const PARAGRAPH_TEMPLATE = [
     [ 'core/paragraph', { className: 'card-text' } ],
 ];
 
-const IMAGE_TEMPLATE = [
-	[ 'core/image', { className: 'card-img-top' } ],
-]
-
 const HEADING_TEMPLATE = [
 	[ 'core/heading', { className: 'card-title' } ],
+];
+
+const TOP_CAP_TEMPLATE = [
+	[ 'core/image', { className: 'card-img-top' } ],
+];
+
+const IMAGE_TEMPLATE = [
+	[ 'core/image', { className: 'card-img' } ],
 ];
 
 // import './style.scss';
 // Commenting out the front style, as it will be handled by the bootstrap css pulled in.
 import './editor.scss';
 
-registerBlockType( 'card/main', {
+const bgColors = [
+	{ name: 'Primary', slug: 'bg-primary', color: '#58595b'},
+	{ name: 'Secondary', slug: 'bg-secondary', color: '#006c93'},
+	{ name: 'Light', slug: 'bg-light', color: '#F6F6F6'},
+	{ name: 'Dark', slug: 'bg-dark', color: '#4b4b4b'},
+];
+
+const borderColors = [
+	{ name: 'Primary', slug: 'border-primary', color: '#58595b'},
+	{ name: 'Secondary', slug: 'border-secondary', color: '#006c93'},
+	{ name: 'Light', slug: 'border-light', color: '#F6F6F6'},
+	{ name: 'Dark', slug: 'border-dark', color: '#4b4b4b'},
+];
+
+const textColors = [
+	{ name: 'Primary', slug: 'text-primary', color: '#58595b'},
+	{ name: 'Secondary', slug: 'text-secondary', color: '#006c93'},
+	{ name: 'Light', slug: 'text-light', color: '#F6F6F6'},
+	{ name: 'Dark', slug: 'text-dark', color: '#4b4b4b'},
+];
+
+/*registerBlockVariation( 'core/image', {
+	name: 'captop',
+	title: 'Image Cap Top',
+	icon: ( <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 18h6v6h-6v-6zm-9 6h6v-6h-6v6zm-9 0h6v-6h-6v6zm0-8h24v-16h-24v16z"/></svg> ),
+	attributes: {
+		className: 'card-img-top',
+		testBlockName: 'capTop',
+		testAttribute: 'Does it work'
+	},
+	isActive: ( blockAttributes, variationAttributes ) => blockAttributes.parentBlockName === variationAttributes.testBlockName
+} );*/
+
+registerBlockType( 'utksds/card', {
 	title: 'Card',
 	icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 0v2h-18v18h-2v-20h20zm-7.281 20.497l-.719 3.503 3.564-.658-2.845-2.845zm8.435-8.436l2.846 2.845-7.612 7.612-2.845-2.845 7.611-7.612zm-17.154-8.061v20h6v-2h-4v-16h16v4.077l2 2v-8.077h-20z"/></svg>,
 	category: 'utdesign_system',
 	description: '',
 	attributes: {
-		backgroundColor: {
-			type: 'string',
-			default: 'gray',
-		},
 		blockName: {
 			type: 'string',
-			default: 'card',
-		}
+			default: 'utksds/card',
+		},
+		cardColor: {
+			type: 'object',
+			default: { name: 'Primary', slug: 'bg-primary', color: '#58595b'},
+		},
+		cardOutline: {
+			type: 'boolean',
+			default: false,
+		},
 	},
 	providesContext: {
     	'card/blockName': 'blockName',
+		'card/cardColor': 'cardColor',
+		'card/cardOutline': 'cardOutline',
 	},
 
 	edit: ( props ) => {
 		//const { backgroundColor } = attributes;
 		const {
-    		attributes: { backgroundColor },
+    		attributes,
 			clientId,
 			name,
     		setAttributes
@@ -66,15 +110,73 @@ registerBlockType( 'card/main', {
 		return ( [
 			// eslint-disable-next-line react/jsx-key
 			<InspectorControls style={ { marginBottom: '40px' } }>
-				<PanelBody title={ 'Background Color Settings' }>
-					<p><strong>Select a Background color:</strong></p>
-					<ColorPalette value={ backgroundColor }
-						onChange={ onBackgroundColorChange } />
+				<PanelBody title='Colors' initialOpen={ true }>
+					{ ! attributes.cardOutline && (
+					<PanelRow>
+						<p><strong>Select a background color:</strong></p>
+					</PanelRow>
+					) }
+					{ ! attributes.cardOutline && (
+					<PanelRow>
+						<ColorPalette 
+							colors = { bgColors }
+							value={ attributes.cardColor.color }
+							onChange={ ( value ) =>{
+								const thisColor = getColorObjectByColorValue( bgColors, value );
+								setAttributes( { cardColor:thisColor } );
+								//console.log(thisColor);
+							} }
+							disableCustomColors={ true }
+							clearable={ false }
+						/>
+					</PanelRow>
+					) }
+					{ attributes.cardOutline && (
+					<PanelRow>
+						<p><strong>Select a border color:</strong></p>
+					</PanelRow>
+					) }
+					{ attributes.cardOutline && (
+					<PanelRow>
+						<ColorPalette 
+							colors = { borderColors }
+							value={ attributes.cardColor.color }
+							onChange={ ( value ) =>{
+								const thisColor = getColorObjectByColorValue( borderColors, value );
+								setAttributes( { cardColor:thisColor } );
+								//console.log(thisColor);
+							} }
+							disableCustomColors={ true }
+							clearable={ false }
+						/>
+					</PanelRow>
+					) }
+					<PanelRow>
+						<ToggleControl
+							label='Card Fill'
+							help={ attributes.cardOutline ? 'Card is outlined in color.' : 'Card is solid color.' }
+							checked={ attributes.cardOutline }
+							onChange={ () => {
+								setAttributes( { cardOutline: !attributes.cardOutline } );
+								//console.log(attributes.buttonOutline);
+								
+								if( !attributes.cardOutline === true ){
+									const thisColor = getColorObjectByColorValue( borderColors, attributes.cardColor.color );
+									setAttributes( { cardColor:thisColor } );
+								}else{
+									const thisColor = getColorObjectByColorValue( bgColors, attributes.cardColor.color );
+									setAttributes( { cardColor:thisColor } );
+								}
+			
+								//console.log(attributes.buttonColor);
+							} }
+						/>
+					</PanelRow>
 				</PanelBody>
 			</InspectorControls>,
 			// eslint-disable-next-line react/jsx-key
-			<div className="card card-edit" style={ { background: backgroundColor } }>
-				<InnerBlocks allowedBlocks={ [ 'card/body', 'card/header', 'card/footer', 'core/image' ] } renderAppender={ () => ( <InnerBlocks.ButtonBlockAppender /> ) } />
+			<div className={'card card-edit ' + attributes.cardColor.slug }>
+				<InnerBlocks allowedBlocks={ [ 'card/body', 'card/image', 'utksds/columns', 'card/overlay', 'card/topcap', ] } renderAppender={ () => ( <InnerBlocks.ButtonBlockAppender /> ) } />
 			</div>,
 		] );
 	},
@@ -83,7 +185,7 @@ registerBlockType( 'card/main', {
 		const { backgroundColor } = attributes;
 
 		return (
-			<div className="card" style={ { background: backgroundColor } }>
+			<div className={'card ' + attributes.cardColor.slug }>
 				<InnerBlocks.Content />
 			</div>
 		);
@@ -94,10 +196,51 @@ registerBlockType( 'card/body', {
 	title: 'Card Body',
 	parent: [ 'card/main' ],
 	icon: 'media-text',
+	description: 'Contains all the text elements within a Card.',
+	usesContext: [ 'card/cardColor', 'card/cardOutline', ],
+	attributes: {
+		textColor: {
+			type: 'string',
+		},
+	},
+				  
+	edit: ( { attributes, context, setAttributes } ) => {
+	
+		if( context['card/cardOutline'] === true ){
+			const thisColor = getColorObjectByColorValue( textColors, context['card/cardColor'].color );
+			setAttributes( { textColor:thisColor.slug } );
+		}
+		
+		if( context['card/cardOutline'] === false ){
+			attributes.textColor = '';
+		}
+	
+		return (
+			<div className={ 'card-body ' + attributes.textColor }>
+				<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } templateLock={ false } renderAppender={ () => ( <InnerBlocks.ButtonBlockAppender /> ) } />
+			</div>
+		)
+	},
+	
+	save: ( { attributes } ) => {
+		return (
+			<div className={ 'card-body ' + attributes.textColor }>
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+			
+} );
+
+registerBlockType( 'card/overlay', {
+	title: 'Card Overlay',
+	parent: [ 'card/main' ],
+	icon: 'media-text',
+	description: 'Contains all the text elements that overlay an image in a Card.',
 				  
 	edit: () => {
 		return (
-			<div className="card-body">
+			<div className="card-img-overlay">
 				<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } templateLock={ false } renderAppender={ () => ( <InnerBlocks.ButtonBlockAppender /> ) } />
 			</div>
 		)
@@ -105,7 +248,7 @@ registerBlockType( 'card/body', {
 	
 	save: () => {
 		return (
-			<div className="card-body">
+			<div className="card-img-overlay">
 				<InnerBlocks.Content />
 			</div>
 		);
@@ -151,57 +294,45 @@ registerBlockType( 'card/heading', {
 			
 } );
 		
-/*registerBlockType( 'card/image', {
+registerBlockType( 'card/image', {
 	title: 'Image',
-	parent: [ 'card/main' ],
-	icon: 'format-image',
-	usesContext: ['card/blockName'],
-	attributes: {
-		imagePostion: {
-			type: 'string',
-			default: 'card-img-top',
-		},
-	},
+	parent: [ 'utksds/card' ],
+	icon: (<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" role="img" aria-hidden="true" focusable="false"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM5 4.5h14c.3 0 .5.2.5.5v8.4l-3-2.9c-.3-.3-.8-.3-1 0L11.9 14 9 12c-.3-.2-.6-.2-.8 0l-3.6 2.6V5c-.1-.3.1-.5.4-.5zm14 15H5c-.3 0-.5-.2-.5-.5v-2.4l4.1-3 3 1.9c.3.2.7.2.9-.1L16 12l3.5 3.4V19c0 .3-.2.5-.5.5z"></path></svg>),
 				  
-	edit: ( { attributes, setAttributes, context } ) => {
-		const { imagePostion } = attributes;
-		
-		//console.log('Current attributes: ', attributes);
-
-		function onImagePositionChange( newValue ) {
-			setAttributes( { imagePostion: newValue } );
-		}
-		
-		return ( [
-			<InspectorControls>
-				<PanelBody title='Image Cap'>
-				<RadioControl
-		label="Image Position"
-		help="The placement of the image on the card."
-		selected={ imagePostion }
-		options={ [
-			{ label: 'Top', value: 'card-img-top' },
-			{ label: 'Bottom', value: 'card-img-bottom' },
-		] }
-		onChange={ onImagePositionChange }
-	/>
-				</PanelBody>
-			</InspectorControls>,
-			<div className={ imagePostion + ' ' + context['card/blockName'] }><InnerBlocks template={ IMAGE_TEMPLATE } allowedBlocks={ 'core/image' } templateLock={ 'all' } /></div>,
-		] );
+	edit: () => {
+		return (
+			<InnerBlocks template={ IMAGE_TEMPLATE } allowedBlocks={ 'core/image' } templateLock={ 'all' } />
+		)
 	},
 	
-	save: ( { attributes } ) => {
-		const { imagePostion } = attributes;
-		
+	save: () => {
 		return (
 			<InnerBlocks.Content />
 		);
 	},
 			
-} );*/
+} );
+		
+registerBlockType( 'card/topcap', {
+	title: 'Image Cap Top',
+	parent: [ 'utksds/card' ],
+	icon: (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 18h6v6h-6v-6zm-9 6h6v-6h-6v6zm-9 0h6v-6h-6v6zm0-8h24v-16h-24v16z"/></svg>),
+				  
+	edit: () => {
+		return (
+			<InnerBlocks template={ TOP_CAP_TEMPLATE } allowedBlocks={ 'core/image' } templateLock={ 'all' } />
+		)
+	},
+	
+	save: () => {
+		return (
+			<InnerBlocks.Content />
+		);
+	},
+			
+} );
 
-registerBlockType( 'card/header', {
+/*registerBlockType( 'card/header', {
 	title: 'Card Header',
 	parent: [ 'card/main' ],
 	icon: 'table-row-before',
@@ -255,4 +386,4 @@ registerBlockType( 'card/footer', {
 	save: ( { className, attributes } ) => {
 		return <RichText.Content tagName="div" className="card-footer card-footer" value={ attributes.content } />;
 	},
-} );
+} );*/
