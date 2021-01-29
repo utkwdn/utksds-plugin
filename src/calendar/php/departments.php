e0d5032485e41fc45aea8e1ef0a1f7f33417d7e2
Charlie Cantrell <ccantr19@utk.edu>
2021-01-29 14:05:24 -0500

    Used the localist API to generate a react const file of the calendar departments.

---
+++ src/calendar/php/departments.php
@@ -0,0 +1,47 @@
+<?php
+
+function get_departments($page = NULL){
+	$pp = '100';
+	
+	if(!isset($page) && $page != ''){
+		$page = '';
+	}
+	
+	$url = 'https://calendar.utk.edu/api/2/departments?pp='.$pp.'&page='.$page;
+	
+	$json = file_get_contents($url);
+	$depts = json_decode($json);
+	
+	return($depts);
+}
+
+$depts = get_departments();
+$x=0;
+foreach($depts->departments as $this_dept){
+	$departments[$x]['urlname'] = $this_dept->department->urlname;
+	$departments[$x]['name'] = $this_dept->department->name;
+	$x++;
+}
+
+if($depts->page->total > 1){
+	$i = 2;
+	while($i <= $depts->page->total){
+		$this_page = get_departments($i);
+		
+		foreach($this_page->departments as $this_dept){
+			$departments[$x]['urlname'] = $this_dept->department->urlname;
+			$departments[$x]['name'] = $this_dept->department->name;
+			$x++;
+		}
+		$i++;
+	}
+}
+
+//print_r($departments);
+
+print 'const departments = [';
+foreach($departments as $department){
+	print "{ label: '".$department['name']."', value: '".$department['urlname']."'},";
+}
+print ']';
+?>
