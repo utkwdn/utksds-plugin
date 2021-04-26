465ed4d1868f270ac3ec42b61b23cf869dc3ece8
Charlie Cantrell <ccantr19@utk.edu>
2021-04-26 11:21:14 -0400

    Changed mediaupload component to mediaplaceholder in overlay block.

--- src/overlay/overlay.js
+++ src/overlay/overlay.js
@@ -5,7 +5,7 @@ import { store as blocksStore } from '@wordpress/blocks';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 
 const { registerBlockType, registerBlockVariation, getBlockVariations, createBlocksFromInnerBlocksTemplate } = wp.blocks;
-const { InnerBlocks, InspectorControls, MediaUpload, ColorPalette, RichText, getColorObjectByColorValue, __experimentalBlockVariationPicker } = wp.blockEditor;
+const { InnerBlocks, InspectorControls, ColorPalette, RichText, getColorObjectByColorValue, MediaPlaceholder, BlockIcon, __experimentalBlockVariationPicker } = wp.blockEditor;
 const { PanelBody, PanelRow, Button, RangeControl, RadioControl, ToggleControl, SelectControl } = wp.components;
 const { withState } = wp.compose;
 
@@ -43,7 +43,7 @@ registerBlockType( 'utksds/overlay', {
 		const { replaceInnerBlocks } = useDispatch( blockEditorStore );
 	
 		const ovPlaceholder = (
-			<MediaUpload
+			<MediaPlaceholder
 				onSelect={ media => {
 					setAttributes( { imageAlt: media.alt, imageUrl: media.url } );
 					
@@ -55,16 +55,12 @@ registerBlockType( 'utksds/overlay', {
 						true
 					);
 				} }
-				type="image"
+				allowedTypes={ [ 'image' ] }
 				value={ attributes.imageID }
-				render={ ( { open } ) =>(
-					<Button onClick={ open }>Open Media Library</Button>
-				) }
-			/>
+				icon={ <BlockIcon icon={ <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-richtext" viewBox="0 0 16 16"><path d="M7 4.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0zm-.861 1.542 1.33.886 1.854-1.855a.25.25 0 0 1 .289-.047l1.888.974V7.5a.5.5 0 0 1-.5.5H5a.5.5 0 0 1-.5-.5V7s1.54-1.274 1.639-1.208zM5 9a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/></svg> } /> }
+				labels={ { title: 'The Image', instructions: 'Upload an image or select one from the media library to be the background of this block.' } }
+				></MediaPlaceholder>
 		);
-
-		console.log(attributes.imageAlt);
-		console.log(attributes.imageUrl);
 
 		return ( [
 			<div className="card">
