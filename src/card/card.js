20d576ec17ff4af7e6b04c18351f57af9129b575
Charlie Cantrell <ccantr19@utk.edu>
2021-05-14 15:06:26 -0400

    Added helper text to card template.

--- src/card/card.js
+++ src/card/card.js
@@ -88,7 +88,7 @@ registerBlockType( 'utksds/card', {
 	variations: [
 	{
 		name: 'image-title-body-button',
-		title: 'Top image with title, text, and button',
+		title: 'Top image, text, and button',
 		description: 'Top image with title, body, and button',
 		icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-heading" viewBox="0 0 16 16"><path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/><path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1z"/></svg>),
 		innerBlocks: [
@@ -101,7 +101,7 @@ registerBlockType( 'utksds/card', {
 	},
 	{
 		name: 'header-footer',
-		title: 'Card with header and footer',
+		title: 'Header and footer',
 		description: 'Card with header and footer',
 		icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-heading" viewBox="0 0 16 16"><path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/><path d="M3 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm0-5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5v-1z"/></svg>),
 		innerBlocks: [ 
@@ -115,7 +115,7 @@ registerBlockType( 'utksds/card', {
 	},
 	{
 		name: 'title-body',
-		title: 'Card with title and text',
+		title: 'Title and text',
 		description: 'Card with title and text',
 		icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16"><path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/><path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/></svg>),
 		innerBlocks: [
@@ -127,7 +127,7 @@ registerBlockType( 'utksds/card', {
 	},
 	{
 		name: 'blank',
-		title: 'Blank card to build from scratch',
+		title: 'Blank',
 		description: 'Blank card to build from scratch',
 		icon: (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/></svg>),
 		innerBlocks: [ 
@@ -152,7 +152,7 @@ registerBlockType( 'utksds/card', {
 		var cardPlaceholder = (
 			<__experimentalBlockVariationPicker
 				label = 'Card Template'
-				instructions = 'Choose a card template or a blank card to build your own.'
+				instructions = 'A card is a flexible and extensible content container for highlighting pieces of content with multiple variants and options. Choose a template to get started.'
 				variations={ cardVariations }
 				onSelect={ ( nextVariation ) =>{
 					//console.log( nextVariation );
