76805d0aa471ea12dc3870001a8f928a6df88a3e
Charlie Cantrell <ccantr19@utk.edu>
2021-05-18 16:00:59 -0400

    MediaPlaceholder implemented in Media Object block.

--- src/media-object/media-object.js
+++ src/media-object/media-object.js
@@ -1,11 +1,23 @@
-const { registerBlockType } = wp.blocks;
-const { MediaUpload, InnerBlocks, InspectorControls, ColorPalette } = wp.blockEditor;
+import { dispatch, withDispatch, useDispatch  } from '@wordpress/data';
+import { select } from '@wordpress/data';
+import { Path, SVG } from '@wordpress/components';
+import { store as blocksStore } from '@wordpress/blocks';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+
+const { registerBlockType, createBlocksFromInnerBlocksTemplate } = wp.blocks;
+const { MediaUpload, BlockIcon, MediaPlaceholder, InnerBlocks, InspectorControls, ColorPalette } = wp.blockEditor;
 const { Button, PanelBody, PanelRow, RangeControl } = wp.components;
-const ALLOWED_BLOCKS = [ 'core/button', 'core/column', 'core/columns', 'core/separator', 'core/paragraph', 'core/heading' ];
+const ALLOWED_BLOCKS = [ 'utksds/button', 'horizontal-rule/main', 'core/paragraph', 'core/heading' ];
 
 // import './style.scss';
 // Commenting out the front style, as it will be handled by the bootstrap css pulled in.
 import './editor.scss';
+
+const MEDIA_TEMPLATE = [
+	[ 'media/content', {}, [
+    	[ 'core/paragraph' ],
+	] ],
+];
 
 registerBlockType( 'media-object/main', {
 	title: 'Media Object',
@@ -32,8 +44,10 @@ registerBlockType( 'media-object/main', {
 	},
 
 	// eslint-disable-next-line no-unused-vars
-	edit( { attributes, setAttributes } ) {
+	edit( { clientId, attributes, setAttributes } ) {
 		const { backgroundColor } = attributes;
+	
+		const { replaceInnerBlocks } = useDispatch( blockEditorStore );
 
 		function onBackgroundColorChange( newColor ) {
 			setAttributes( { backgroundColor: newColor } );
@@ -70,6 +84,26 @@ registerBlockType( 'media-object/main', {
 				</div>
 			);
 		};
+	
+		const ovPlaceholder = (
+			<MediaPlaceholder
+				onSelect={ media => {
+					setAttributes( { imageAlt: media.alt, imageUrl: media.url, imageSize: media.width } );
+					
+					replaceInnerBlocks(
+						clientId,
+						createBlocksFromInnerBlocksTemplate(
+							MEDIA_TEMPLATE
+						),
+						true
+					);
+				} }
+				allowedTypes={ [ 'image' ] }
+				value={ attributes.imageID }
+				icon={ <BlockIcon icon={ <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 5h-8v2h8v-2zm0 4h-8v2h8v-2zm0 4h-8v2h8v-2zm0 4h-8v2h8v-2zm-10 1.597l-.003.403h-13.994l-.003-.403c0-.896.116-1.937 1.854-2.335 1.729-.396 3.838-1.421 3.217-2.755-.397.64-1.635 1.037-2.597.469 2.771-.702-1.36-5.524 2.258-8.228.649-.5 1.449-.748 2.253-.748.809 0 1.62.251 2.283.748 3.618 2.704-.512 7.526 2.258 8.228-.962.568-2.198.188-2.596-.469-.622 1.333 1.484 2.358 3.216 2.755 1.737.399 1.854 1.439 1.854 2.335z"/></svg> } /> }
+				labels={ { title: 'The Image', instructions: 'Upload an image or select one from the media library to showcase.' } }
+				></MediaPlaceholder>
+		);
 
 		return ( [
 			<InspectorControls>
@@ -84,6 +118,7 @@ registerBlockType( 'media-object/main', {
 				</PanelBody>
 			</InspectorControls>,
 			<div className="media">
+				{ attributes.imageUrl && (
 				<MediaUpload
 					onSelect={ media => {
 						setAttributes( { imageAlt: media.alt, imageUrl: media.url, imageSize: media.width } );
@@ -92,7 +127,8 @@ registerBlockType( 'media-object/main', {
 					value={ attributes.imageID }
 					render={ ( { open } ) => getImageButton( open ) }
 				/>
-				<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } />
+				) }
+				<InnerBlocks allowedBlocks={ [ 'media/content' ] } placeholder={ ovPlaceholder } templateLock={ true } />
 			</div>
 		] );
 	},
@@ -135,6 +171,26 @@ registerBlockType( 'media-object/main', {
 					<InnerBlocks.Content />
 				</div>
 			</div>
+		);
+	},
+} );
+
+registerBlockType( 'media/content', {
+	title: 'Media Content',
+	parent: [ 'media-object/main' ],
+	description: 'Inner blocks container for Media Object.',
+	
+	edit: ( props ) => {
+		
+		return(
+			<InnerBlocks allowedBlocks={ ALLOWED_BLOCKS } templateLock={ false } />
+		);
+	},
+	
+	save: ( { attributes } ) => {
+
+		return (
+			<InnerBlocks.Content />
 		);
 	},
 } );
