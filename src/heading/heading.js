7f7e9c27be4fb2ff97ca4e712cecdd9916033cc7
Charlie Cantrell <ccantr19@utk.edu>
2021-06-14 14:33:09 -0400

    Updated core Heading block descriptive text.

--- src/heading/heading.js
+++ src/heading/heading.js
@@ -30,6 +30,9 @@ function addSupports( settings ) {
 	/*check if object exists for old Gutenberg version compatibility
 	add allowedBlocks restriction*/
 	if( typeof settings.supports !== 'undefined' && allowedBlocks.includes( settings.name ) ){
+		
+		console.log(settings);
+		
 		settings.supports = Object.assign( settings.supports, {
 			defaultStylePicker:false,
 			fontSize:false,
@@ -38,6 +41,8 @@ function addSupports( settings ) {
 				text:false,
 			},
 		});
+		
+		settings.description = "Traditional heading elements are designed to work best in the meat of your page content. When you need a heading to stand out, consider using a display heading — a larger, slightly more opinionated heading style."
 	}
 	
 	return settings;
