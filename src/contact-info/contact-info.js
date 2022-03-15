1fa4f56a1c57d684c679e4c3def0db1660267492
Charlie Cantrell <ccantr19@utk.edu>
2022-03-15 15:22:15 -0400

    Changed links to white in footer contact info

--- src/contact-info/contact-info.js
+++ src/contact-info/contact-info.js
@@ -164,7 +164,7 @@ registerBlockType( 'utksds/contact', {
 				<div className="address col-auto">
 				<svg class="meta-address" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"></path></svg>
 				<a
-					className={ 'map-link' }
+					className={ 'text-white map-link' }
 					href={ attributes.url }
 					target={ attributes.linkTarget }
 				>
@@ -186,7 +186,7 @@ registerBlockType( 'utksds/contact', {
 				{ attributes.email !== undefined && (
 				<small className="emailList">Email:&nbsp; 
 				<a
-					className={ 'email text-reset' }
+					className={ 'email text-white text-reset' }
 					href={ 'mailto:' + attributes.email }
 					target={ attributes.linkTarget }
 				>
@@ -312,7 +312,7 @@ registerBlockType( 'utksds/phone', {
 				/>:&nbsp;
 			
 				<a
-					className={ 'tel' }
+					className={ 'text-white tel' }
 					href={ 'tel:' + attributes.phoneNum }
 					target={ attributes.linkTarget }
 				>
